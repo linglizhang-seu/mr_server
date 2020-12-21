dc018f523898be05453c9299d71dec80151887c3
BabaBoogey <49062842+BabaBoogey@users.noreply.github.com>
2020-12-21 15:14:40 +0800

    add logfile

--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
 #include <QCoreApplication>
 #include <stdlib.h>
+#include <QMutex>
+#include <QDateTime>
+#include <QFile>
+#include <cstdlib>
+#include <cstdio>
 #include "manageserver.h"
 //传入的apo需要重新保存，使得n按顺序
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    // 加锁
+    static QMutex mutex;
+    mutex.lock();
+
+    QByteArray localMsg = msg.toLocal8Bit();
+
+//    QString strMsg("");
+//    switch(type)
+//    {
+//    case QtDebugMsg:
+//        strMsg = QString("Debug:");
+//        break;
+//    case QtWarningMsg:
+//        strMsg = QString("Warning:");
+//        break;
+//    case QtCriticalMsg:
+//        strMsg = QString("Critical:");
+//        break;
+//    case QtFatalMsg:
+//        strMsg = QString("Fatal:");
+//        break;
+//    }
+
+    // 设置输出信息格式
+    QString strDateTime = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss ddd");
+    QString strMessage = QString("Message:%1 File:%2  Line:%3  Function:%4  DateTime:%5")
+            .arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function).arg(strDateTime);
+
+    // 输出信息至文件中（读写、追加形式）
+    QFile file("log.txt");
+    file.open(QIODevice::ReadWrite | QIODevice::Append);
+    QTextStream stream(&file);
+    stream << strMessage << "\r\n";
+    file.flush();
+
+    file.close();
+
+
+    // 解锁
+    mutex.unlock();
+    fprintf(stderr,strMessage.toStdString().c_str());
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(myMessageOutput);
     QCoreApplication a(argc, argv);
+
     ManageServer server;
     if(!server.listen(QHostAddress::Any,26371))
     {
