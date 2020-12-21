2f6aa6ac7c3071810e8d1ae98b35d3d12308ce46
BabaBoogey <49062842+BabaBoogey@users.noreply.github.com>
2020-12-21 15:42:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
 
     // 设置输出信息格式
     QString strDateTime = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss ddd");
-    QString strMessage = QString("Message:%1 File:%2  Line:%3  Function:%4  DateTime:%5")
+    QString strMessage = QString("Message:%1 File:%2  Line:%3  Function:%4  DateTime:%5\n")
             .arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function).arg(strDateTime);
 
     // 输出信息至文件中（读写、追加形式）
