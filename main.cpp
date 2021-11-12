b71f89ed2d750474eac16f785f30cf3c755c182f
BabaBoogey <49062842+BabaBoogey@users.noreply.github.com>
2021-11-12 21:30:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     ManageServer server;
-    if(!server.listen(QHostAddress::Any,26371))
+    if(!server.listen(QHostAddress::Any,26375))
     {
         qDebug()<<"Error:cannot start server in port 9999,please check!";
         exit(-1);
