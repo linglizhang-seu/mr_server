481f8099b320b35f9a20de455455c7ce711b9ba2
BabaBoogey <49062842+BabaBoogey@users.noreply.github.com>
2022-01-26 13:03:37 +0800

    add

--- main.cpp
+++ main.cpp
@@ -26,16 +26,26 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QFile f("/Users/huanglei/Desktop/allsoma.csv");
+    QMap<QString,QVector<int>> mmap;
+    mmap["191799"]={1492,1925,689};
+    mmap["191798"]={1180,1925,669};
+    mmap["191797"]={1120,1925,699};
     QMap<QString,QList<CellAPO>> hashmap;
     if(f.open(QIODevice::ReadOnly)){
         QStringList somalist=QString(f.readAll()).split('\n',Qt::SkipEmptyParts);
         for(auto &row:somalist){
             auto somainfo=row.split(',',Qt::SkipEmptyParts);
             CellAPO m;
-            m.x=somainfo[1].toDouble();
-            m.y=somainfo[2].toDouble();
-            m.z=somainfo[3].toDouble();
+            m.x=somainfo[1].toDouble()/16;
+            m.y=somainfo[2].toDouble()/16;
+            m.z=somainfo[3].toDouble()/16;
             m.color.r=0;m.color.b=0;
+            if(mmap.contains(somainfo[0].trimmed())){
+                if(m.x>=mmap[somainfo[0].trimmed()].at(0)||m.y>=mmap[somainfo[0].trimmed()].at(1)||m.z>=mmap[somainfo[0].trimmed()].at(2)){
+                    qDebug()<<row;
+                    continue;
+                }
+            }
             hashmap[somainfo[0].trimmed()].push_back(m);
         }
     }
